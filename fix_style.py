7053dd974ff4362e5aa2130b3b64b5b3d0add77c
cyj265 <cyj265@users.noreply.github.com>
2026-09-14 15:52:58 +0800

    移除误提交的敏感文件与本地脚本，加入 .gitignore

--- fix_style.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import io
-
-p = r'app\src\mobile\res\values\styles.xml'
-s = io.open(p, encoding='utf-8').read()
-s = s.replace('android:shapeAppearance', 'shapeAppearance')
-io.open(p, 'w', encoding='utf-8', newline='').write(s)
-print('shapeAppearance 前缀已修')
