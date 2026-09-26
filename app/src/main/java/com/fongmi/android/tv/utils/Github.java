7ce5e1fc9f246e499f8aca304ed6f2d72e7c9bf5
cyj265 <cyj265@users.noreply.github.com>
2026-09-26 13:08:34 +0800

    基于上游FongMi/TV 5.6.3移植揽星功能（方案B）

---
+++ app/src/main/java/com/fongmi/android/tv/utils/Github.java
@@ -0,0 +1,8 @@
+package com.fongmi.android.tv.utils;
+
+public class Github {
+
+    public static final String RELEASE = "https://api.github.com/repos/cyj265/lanxing-vod/releases/latest";
+    public static final String RELEASE_MIRROR = "https://gh-proxy.com/https://api.github.com/repos/cyj265/lanxing-vod/releases/latest";
+    public static final String DOWNLOAD_MIRROR_PREFIX = "https://gh-proxy.com/";
+}
